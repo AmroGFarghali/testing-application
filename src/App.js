00f83b9797c94695d393671120bcbd95cf5de684
Amr Gamal <57310811+AmroGFarghali@users.noreply.github.com>
2024-02-25 18:13:12 +0300

    first-push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import {
+  FormGroup,
+  TextInput,
+  RadioButtonGroup,
+  RadioButton,
+  Button,
+  Stack,
+} from "@carbon/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <FormGroup
+      style={{
+        maxWidth: "400px",
+      }}
+      legendText="FormGroup Legend"
+    >
+      <Stack gap={7}>
+        <TextInput data-testid="first name" id="one" labelText="First Name" />
+        <TextInput data-testid="last name" id="two" labelText="Last Name" />
+        <RadioButtonGroup
+          legendText="Radio button heading"
+          name="radio-button-group"
+          defaultSelected="radio-1"
+          data-testid="radio button group"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <RadioButton
+            data-testid="radiobutton"
+            labelText="Option 1"
+            value="radio-1"
+            id="radio-1"
+          />
+          <RadioButton
+            data-testid="radiobutton"
+            labelText="Option 2"
+            value="radio-2"
+            id="radio-2"
+          />
+          <RadioButton
+            data-testid="radiobutton"
+            labelText="Option 3"
+            value="radio-3"
+            id="radio-3"
+          />
+        </RadioButtonGroup>
+        <Button>Submit</Button>
+      </Stack>
+    </FormGroup>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+// Tests that see if components are rendered
+test("checks if both input fields were rendered", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByTestId(/first name/i)).toBeInTheDocument();
+  expect(screen.getByTestId(/last name/i)).toBeInTheDocument();
+});
+
+test("checks if radio buttons are rendered and there are 3 buttons", () => {
+  render(<App />);
+
+  const radioButtons = screen.getAllByTestId(/radiobutton/i);
+  radioButtons.forEach((button) => {
+    expect(button).toBeInTheDocument();
+  });
+  expect(radioButtons).toHaveLength(3);
+});
+//Input fields are empty by default
+test("first and last name input should be empty", () => {
+  render(<App />);
+  const firstName = screen.getByTestId(/first name/i);
+  const lastName = screen.getByTestId(/last name/i);
+  expect(firstName.value).toBe("");
+  expect(lastName.value).toBe("");
+});
+//Input fields reflect change when typing
+test("first name input should change", () => {
+  render(<App />);
+  const firstName = screen.getByTestId(/first name/i);
+  const testValue = "test";
+
+  fireEvent.change(firstName, { target: { value: testValue } });
+  expect(firstName.value).toBe(testValue);
+});
+test("last name input should change", () => {
+  render(<App />);
+  const lastName = screen.getByTestId(/last name/i);
+  const testValue = "test";
+
+  fireEvent.change(lastName, { target: { value: testValue } });
+  expect(lastName.value).toBe(testValue);
+});
+
+//Checks to see if radio buttons are all unchecked expect button 1 which is checked by default
+test("checks to see if first button is checked and rest isnt", () => {
+  render(<App />);
+
+  const radioButtons = screen.getAllByTestId(/radiobutton/i);
+  radioButtons.forEach((button, index) => {
+    if (index === 0) expect(button.defaultChecked).toBe(true);
+    else expect(button.defaultChecked).toBe(false);
+  });
+  expect(radioButtons).toHaveLength(3);
 });
